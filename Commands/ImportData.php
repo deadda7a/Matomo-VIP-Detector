2d4fc9712614a29540decb7e65eeec2e5ffb8446
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2023-11-17 17:45:11 +0100

    don't overwrite execute function in command

--- Commands/ImportData.php
+++ Commands/ImportData.php
@@ -2,17 +2,14 @@
 namespace Piwik\Plugins\VipDetector\Commands;
 
 use Piwik\Plugin\ConsoleCommand;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Output\OutputInterface;
 use Piwik\Plugins\VipDetector\Dao\DatabaseMethods;
 use Piwik\Plugins\VipDetector\libs\Helpers;
 
 class ImportData extends ConsoleCommand
 {
-    var $_output; // Console output handler
-
-    protected function configure() {
+    protected function configure(): void
+    {
         $this->setName('vipdetector:import-data');
         $this->setDescription('Import Json File with ranges');
         $this->addArgument(
@@ -22,22 +19,24 @@ class ImportData extends ConsoleCommand
         );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        $this->_output = $output;
+    protected function doExecute(): int {
+        $output = $this->getOutput();
+        $input = $this->getInput();
+
         $file = $input->getArgument('file');
 
-        $this->logMessage(sprintf('Loading from file %s', $file));
+        $output->writeln(sprintf('Loading from file %s', $file));
 
         // File not found etc
         if(!$string = @file_get_contents($file)) {
-            $this->logMessage('Could not load file!', "error");
-            exit(1);
+            $output->writeln('Could not load file!', "error");
+            return self::FAILURE;
         };
 
         // File is not valid json
         if (!$json = json_decode($string)) {
-            $this->logMessage('Could not parse file!', "error");
-            exit(1);
+            $output->writeln('Could not parse file!', "error");
+            return self::FAILURE;
         }
 
         // Loop through the file
@@ -45,7 +44,7 @@ class ImportData extends ConsoleCommand
             // Check if the name is already in the database and insert it if it's not there
             // (should not happen, since they are supposed to be unique, but useful when the file is extended and re-imported)
             if (!DatabaseMethods::checkNameInDb('vip_detector_names', $entry->name)) {
-                $this->logMessage(sprintf("Inserting %s", $entry->name));
+                $output->writeln(sprintf("Inserting %s", $entry->name));
                 DatabaseMethods::insertName($entry->name);
             }
 
@@ -59,7 +58,7 @@ class ImportData extends ConsoleCommand
                     // We need to call this a 2nd time, because we also need the ID if no name was inserted in this run
                     $nameId = DatabaseMethods::checkNameInDb('vip_detector_names', $entry->name);
                     // We need the corresponding name id from the other table, and we want everything as one array
-                    $this->logMessage(
+                    $output->writeln(
                         sprintf(
                             'Inserting Range %s to %s with Name ID %s',
                             $rangeInfo['range_from'],
@@ -77,23 +76,7 @@ class ImportData extends ConsoleCommand
                 }
             }
         }
-    }
 
-    private function logMessage(string $message, string $level = 'info') {
-        if ($level == 'error') {
-            $this->_output->writeln(
-                sprintf(
-                    '<error>Matomo VIP Detector ImportData: %s</error>',
-                    $message
-                )
-            );
-        } else {
-            $this->_output->writeln(
-                sprintf(
-                    '<info>Matomo VIP Detector ImportData: %s</info>',
-                    $message
-                )
-            );
-        }
+        return self::SUCCESS;
     }
 }
