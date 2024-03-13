0544241b4cb393235cef68296664629200bc363d
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2024-03-13 18:39:47 +0100

    add empty tests

---
+++ tests/Integration/ImportUrlTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Piwik\Plugins\VipDetector\tests\Integration;
+
+use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
+
+/**
+ * @group VipDetector
+ * @group ImportUrlTest
+ * @group Plugins
+ */
+class ImportUrlTest extends IntegrationTestCase {
+    public function setUp(): void {
+        parent::setUp();
+
+        // set up your test here if needed
+    }
+
+    public function tearDown(): void {
+        // clean up your test here if needed
+
+        parent::tearDown();
+    }
+
+    /**
+     * All your actual test methods should start with the name "test"
+     */
+    public function testSimpleAddition() {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
