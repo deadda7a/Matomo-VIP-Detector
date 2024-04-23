cdfd98a2911283d252c67627b90929f1cb682019
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2024-04-23 09:28:18 +0000

    Tests: Xdebug is already enabled

--- tests/github-enable-xdebug.sh
+++ tests/github-enable-xdebug.sh
@@ -2,4 +2,4 @@
 
 sudo pecl install xdebug
 sudo phpenmod xdebug
-echo -e "zend_extension=xdebug\nxdebug.mode=coverage" | sudo tee -a "$(cut -d '"' -f 2 < <(php --ini | grep '(php.ini)' | sed -e "s|.*: s*||"))/php.ini"
+echo "xdebug.mode=coverage" | sudo tee -a "$(cut -d '"' -f 2 < <(php --ini | grep '(php.ini)' | sed -e "s|.*: s*||"))/php.ini"
