6004f44ecf731912e73c018a0f40b72ca325d197
Sebastian Elisa Pfeifer <sebastian.pfeifer@tuwien.ac.at>
2024-12-16 13:09:03 +0100

    test with matomo 5.2.0

--- tests/download-matomo.sh
+++ tests/download-matomo.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/matomo-org/matomo /tmp/matomo
 git clone https://github.com/matomo-org/component-network.git /tmp/matomo-net
 cd /tmp/matomo || exit 1
-git checkout 5.0.3
+git checkout 5.2.0
 git submodule update --init
 composer install --prefer-dist --no-progress
